c2b764d304cb0290cc5be30e9b055f72c5bdec2e
bloomy215 <bmbloomquist@gmail.com>
2019-05-02 23:21:20 -0500

    fixed navbar, 1 down, infinity to go

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,23 +1,23 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg = "light" expand = "lg">
       <Navbar.Brand>Google Book Search</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
+      <Navbar.Toggle aria-controls = "basic-navbar-nav" />
+      <Navbar.Collapse id = "basic-navbar-nav">
+        <Nav className = "mr-auto">
           {/* <Nav.Link href="#home">Search</Nav.Link> */}
-          <Nav.Link to = "/" className = { window.location.pathname === "/" ? "navl-link active" : "nav-link" }>
+          <Link to = "/" className = "m-2">
             Search
-          </Nav.Link>
+          </Link>
           {/* <Nav.Link href="#saved">Saved</Nav.Link> */}
-          <Nav.Link to = "/saved" className = { window.location.pathname === "/saved" ? "navl-link active" : "nav-link" }>
+          <Link to = "/saved" className = "m-2">
             Saved
-          </Nav.Link>
+          </Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
